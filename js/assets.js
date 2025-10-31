d8ec8ba45c7938fbbd9c04c8fb9736d8804752ec
jcamilogarciacano <juancamigarcia@gmail.com>
2025-10-31 18:39:51 +0100

    Add dynamic hero and carousel with asset management
    Introduces js/assets.js to manage banner and hero images. Refactors landing and index pages to dynamically render carousels and hero visuals using assets from the new script, improving maintainability and enabling image rotation. Updates CSS for new hero and carousel styles, accessibility, and interaction enhancements.

---
+++ js/assets.js
@@ -0,0 +1,15 @@
+(function () {
+  const banners = [
+    "img/Banners/20251031_1806_Impresiones en Camisetas_simple_compose_01k8xkmn4zf3499pxxwtwrs3yz.png"
+  ];
+
+  const storeHero = [
+    "img/Tienda/20251031_1806_Impresiones en Camisetas_simple_compose_01k8xkmn4xeghtvrm63cntc4zr.png",
+    "img/Tienda/20251031_1810_Colorful T-Shirt Printing_remix_01k8xkvxspem6bq0ns7s7xwerm.png"
+  ];
+
+  window.siteAssets = Object.assign({}, window.siteAssets, {
+    banners,
+    storeHero
+  });
+})();
